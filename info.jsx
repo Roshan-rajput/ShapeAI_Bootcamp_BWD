bc75d6b82f16c536626333d6d1272a79854f2492
Roshan-rajput <66887147+Roshan-rajput@users.noreply.github.com>
2021-06-28 03:47:20 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+  return (
+    <div className = "note">
+    <h1>Javascript anD React.js </h1>
+    <p>A basic web dev  React JS Bootcamp </p></div>
+  );
+}
+
+export default Footer; 
